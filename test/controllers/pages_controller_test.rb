5f60430f4ba7628aeb4d12e633c5b50ba47eebee
khinezarthwe <koalakhine@gmail.com>
2016-05-08 17:53:57 +0900

    Adding Page with testing

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -4,11 +4,17 @@ class PagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title","Home| Music Recommendation"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title","Help| Music Recommendation"
   end
-
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title","About| Music Recommendation"
+  end
 end
