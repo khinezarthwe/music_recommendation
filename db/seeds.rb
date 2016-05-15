90e4686f7990707a830e78bfff274d6042f0c4d9
khinezarthwe <koalakhine@gmail.com>
2016-05-15 16:04:29 +0900

    Finish user edit , update, index, and destroy actions

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!(name:  "Khine Zar Thwe",
+             email: "koalakhine@gmail.com",
+             password:              "khinezarthwe",
+             password_confirmation: "khinezarthwe",
+             age: 25,
+             admin:true )
+
+99.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password,
+               age: '26')
+end
