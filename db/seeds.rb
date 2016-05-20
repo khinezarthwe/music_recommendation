7755bb0d00ff452816650058a3ba1e919dec6b7a
khinezarthwe <koalakhine@gmail.com>
2016-05-20 22:09:56 +0900

    Changing mail

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 User.create!(name:  "Khine Zar Thwe",
-             email: "koalakhine@gmail.com",
+             email: "khinezar.awitd@gmail.com",
              password:              "khinezarthwe",
              password_confirmation: "khinezarthwe",
              age: 25,
