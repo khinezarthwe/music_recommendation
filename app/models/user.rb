cfc691370cd5f12c6c91d4c96894e3b8554dd4a7
khinezarthwe <koalakhine@gmail.com>
2016-05-20 21:43:01 +0900

    add password resets and email configuartion

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,28 +1,28 @@
 class User < ActiveRecord::Base
-  attr_accessor :remember_token, :activation_token
-	before_save :downcase_email
+  attr_accessor :remember_token, :activation_token, :reset_token
+  before_save :downcase_email
   before_create :create_activation_digest
-  
-	validates :name,  presence: true, length: { maximum: 50 }
+
+  validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
-                    format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: {case_sensitive: false}
-   has_secure_password
-   validates :password, presence: true, length: { minimum: 6 },allow_nil: true
-   VALID_AGE_REGEX = /\d/
-   validates :age, format: {with: VALID_AGE_REGEX}
-   # Returns the hash digest of the given string.
-   def User.digest(string)
-     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                  BCrypt::Engine.cost
+    format: { with: VALID_EMAIL_REGEX },
+    uniqueness: {case_sensitive: false}
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 },allow_nil: true
+  VALID_AGE_REGEX = /\d/
+  validates :age, format: {with: VALID_AGE_REGEX}
+  # Returns the hash digest of the given string.
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+      BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
-   end
-   #Return a random token
-   def User.new_token
-     SecureRandom.urlsafe_base64
-   end
-   # Remembers a user in the database for use in persistent sessions.
+  end
+  #Return a random token
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+  # Remembers a user in the database for use in persistent sessions.
   def remember
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
@@ -42,15 +42,32 @@ class User < ActiveRecord::Base
     return false if digest.nil?
     BCrypt::Password.new(digest).is_password?(token)
   end
-  #Actives an account
+  # Activates an account.
   def activate
-    update_attribute(:activated, true)
-    update_attribute(:activated_at,Time.zone.now)
+    update_attribute(:activated,    true)
+    update_attribute(:activated_at, Time.zone.now)
   end
-  #Sends activation email
+
+  # Sends activation email.
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
   end
+
+  #sets the password reset attributes
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest, User.digest(reset_token))
+    update_attribute(:reset_sent_at,Time.zone.now)
+  end
+  #Sends password reset email
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+  
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
+
   private
   #convert email to all lower-case
   def downcase_email
@@ -62,6 +79,6 @@ class User < ActiveRecord::Base
     self.activation_digest = User.digest(activation_token)
   end
 
-  
-end
 
+
+end
