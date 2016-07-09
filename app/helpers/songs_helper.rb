4d08e09aaa8b0e5880cd0469c918492c6dfce8e1
khinezarthwe <koalakhine@gmail.com>
2016-07-09 23:27:28 +0900

    add youtube https

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -9,6 +9,6 @@ module SongsHelper
       youtube_id = $5
     end
 
-    %Q{<iframe title="YouTube video player" width="640" height="390" src="http://www.youtube.com/embed/#{ youtube_id }" frameborder="0" allowfullscreen></iframe>}
+    %Q{<iframe title="YouTube video player" width="640" height="390" src="https://www.youtube.com/embed/#{ youtube_id }" frameborder="0" allowfullscreen></iframe>}
   end
 end
