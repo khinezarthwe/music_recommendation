071ada1b0428f949eb0af1b11e94be4e5fc8e776
khinezarthwe <koalakhine@gmail.com>
2016-08-11 15:07:58 +0900

    modifying sidekiq file

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -11,6 +11,7 @@ Sidekiq.configure_client do |config|
   if heroku
     config.client_middleware do |chain|
       chain.add Autoscaler::Sidekiq::Client, 'default' => heroku
+      config.redis = { :size => 1 }
     end
   end
 end
@@ -20,6 +21,7 @@ Sidekiq.configure_server do |config|
     if heroku
       p "[Sidekiq] Running on Heroku, autoscaler is used"
       chain.add(Autoscaler::Sidekiq::Server, heroku, 60) # 60 seconds timeout
+      config.redis = { :size => 2 }
     else
       p "[Sidekiq] Running locally, so autoscaler isn't used"
     end
