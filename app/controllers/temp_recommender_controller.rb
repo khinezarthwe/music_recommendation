357394946c0c03aa15455952058c99b18743c684
khinezarthwe <koalakhine@gmail.com>
2016-10-27 14:59:03 +0630

    Merge 2 recommendations into one worker, show them in two column layout

--- app/controllers/temp_recommender_controller.rb
+++ app/controllers/temp_recommender_controller.rb
@@ -2,16 +2,28 @@ class TempRecommenderController < ApplicationController
   def new
   end
   def recommend_song
-  	arr_song = []
+    arr_songs = []
+    genre_songs = []
     RecommendWorker.perform_async(current_user.id)
     temp_recommend_song = TempRecommender.where(:user_id=>current_user.id)
     if temp_recommend_song.nil?
-    	@recommend_song = nil
+      @recommend_song = []
     else
       temp_recommend_song.each do |song|
-        arr_song << song.song_id
+        arr_songs << song.song_id
       end
-      @recommend_song = Song.where(:id=>arr_song)
+      @recommend_song = Song.where(:id=>arr_songs)
+    end
+
+    genre_recommend_song = GenreBasedRecommendation.where(:user_id=>current_user.id)
+
+    if genre_recommend_song.nil?
+      @genre_recommend_song = []
+    else
+      genre_recommend_song.each do |song|
+        genre_songs << song.song_id
+      end
+      @genre_recommend_song = Song.where(:id=>genre_songs)
     end
     respond_to do|format|
       format.html
--- app/workers/recommend_worker.rb
+++ app/workers/recommend_worker.rb
@@ -3,7 +3,8 @@ class RecommendWorker
   include Sidekiq::Worker
   def perform(userid)
     hash_arr = {}
-    songs_by_group = {}
+    songs_by_topic = {}
+    songs_by_genre = {}
     recommend_song = {}
     songs = Song.all
     rem_userid = User.pluck(:id)
@@ -12,36 +13,55 @@ class RecommendWorker
     rem_userid.each do |record_id|
       hash_arr[record_id] = Impression.where(:user_id => record_id).uniq.pluck(:impressionable_id)
     end
-    recommender = CourseRecommender.new
-    recommender.clean!
+    c_recommender = CourseRecommender.new
+    g_recommender = GenreRecommender.new
+    g_recommender.clean!
     hash_arr.each do |user_num,song_id|
-      recommender.add_to_matrix!(:users,user_num.to_s,song_id.map(&:to_s))
+      c_recommender.add_to_matrix!(:users , user_num.to_s, song_id.map(&:to_s))
+      g_recommender.add_to_matrix!(:gusers, user_num.to_s, song_id.map(&:to_s))
     end
-    songs_by_group = songs.group_by{|s| s.topic_num}
-    topic_num = songs_by_group.keys
-    song_group = songs_by_group.map {|k, v| v.map {|i|i.id} }
-    songs_by_group = Hash[topic_num.zip song_group]
-    songs_by_group.each do |k,v|
-      p 'DEBUG'
-      p k, v
-      recommender.add_to_matrix!(:groups,k.to_s,v.map(&:to_s))
+    songs_by_topic = songs.group_by{ |s| s.topic_num  }
+    songs_by_genre = songs.group_by{ |s| s.song_genre }
+
+
+    topic_num  = songs_by_topic.keys
+    song_genre = songs_by_genre.keys
+
+    song_group_topic = songs_by_topic.map {|k, v| v.map {|i|i.id} }
+    song_group_genre = songs_by_genre.map {|k, v| v.map {|i|i.id} }
+
+    songs_by_topic = Hash[topic_num.zip song_group_topic]
+    songs_by_genre = Hash[song_genre.zip song_group_genre]
+
+    songs_by_topic.each do |k,v|
+      c_recommender.add_to_matrix!(:groups, k.to_s, v.map(&:to_s))
     end
-   recommend_song = recommender.predictions_for(userid,matrix_label: :users,with_scores: true)
-   recommend_song = recommend_song.first(20).to_h
-    already_member = TempRecommender.where(:user_id=>userid)
-    if already_member.blank? # create new recommend .nil ???? .blank???
-      recommend_song.each do |rem_song|
-        a = TempRecommender.new(user_id:userid,song_id:rem_song[0],recommend_value:rem_song[1])
-        a.save!
-      end
-    else
-      #save the new one and delete the old one
-      TempRecommender.where(:user_id=>userid).delete_all
-      recommend_song.each do |rem_song|
-        a = TempRecommender.new(user_id:userid,song_id:rem_song[0],recommend_value:rem_song[1])
-        a.save!
-      end
+    songs_by_genre.each do |k,v|
+      g_recommender.add_to_matrix!(:genre_groups, k.to_s, v.map(&:to_s))
     end
+
+    topic_recommendations = c_recommender.predictions_for(userid,matrix_label: :users,with_scores: true)
+    genre_recommendations = g_recommender.predictions_for(userid, matrix_label: :gusers, with_scores: true)
+
+    topic_recommendations = topic_recommendations.first(20).to_h
+    genre_recommendations = genre_recommendations.first(20).to_h
+
+    existing_topic_recommendations = TempRecommender.where(:user_id=>userid)
+    existing_genre_recommendations = GenreBasedRecommendation.where(:user_id=>userid)
+
+    TempRecommender.where(:user_id=>userid).delete_all unless existing_topic_recommendations.empty?
+    GenreBasedRecommendation.where(:user_id=>userid).delete_all unless existing_genre_recommendations.empty?
+
+    topic_recommendations.each do |rem_song|
+      a = TempRecommender.new(user_id:userid,song_id:rem_song[0],recommend_value:rem_song[1])
+      a.save!
+    end
+
+    genre_recommendations.each do |rem_song|
+      a = GenreBasedRecommendation.new(user_id:userid,song_id:rem_song[0],recommend_value:rem_song[1])
+      a.save!
+    end
+
   end
   #recommender.add_to_matrix!(:groups,"group-1","s-1", "s-2","s-5")
   #recommender.predictions_for("user-1", matrix_label: :users,with_scores: true)
